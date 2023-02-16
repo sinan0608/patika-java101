af3d9f8dfd5edbf0ff72ededcc71ac252637f033
Sinan <118401344+sinan0608@users.noreply.github.com>
2023-02-16 16:35:18 +0300

    Recursive us hesaplama

--- UsHesaplama.java
+++ UsHesaplama.java
@@ -2,31 +2,29 @@
 import java.util.Scanner;
 
 public class UsHesaplama {
-	static int us(int a, int b) {
-		int hesap=1;
-		for (int i=1; i<=b ; i++) {
-			hesap *=a;
-		}
-		return hesap;
-	}
-	
-	public static void main(String[] args) {
-		Scanner deger = new Scanner(System.in);
-		
-		while(true) {
-			int a,b;
-			System.out.print("Taban degerini giriniz :");
-			a = deger.nextInt();
-			System.out.print("Üs degerini giriniz :");
-			b = deger.nextInt();
-			if (a==0 && b==0) {
-				System.out.println("Belirsizlik");
-				break;
-			}
-			System.out.println("Sonuç = "+ us(a,b));
-			
-		}		
-	}
+		static boolean asal(int n, int i) {
+        if (n <= 1) {
+            return false;
+        }
+        if (i == 1) {
+            return true;
+        }
+        if (n % i == 0) {
+            return false;
+        }
+        return asal(n,i-1);
+    }
+
+    public static void main(String[] args) {
+    	Scanner scan = new Scanner(System.in);
+    	System.out.println("Lütfen Bir Sayı Girin:");
+        int n = scan.nextInt();
+        int i = n / 2;
+        if (asal(n, i)) {
+            System.out.println(n + " asal sayıdır ");
+        } else {
+            System.out.println(n + " asal sayı değildir");
+        }
+    }
+
 }
-
-
