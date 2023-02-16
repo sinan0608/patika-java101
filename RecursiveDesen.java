68a77a1493b1b2e7e56905611bbae58cd844f216
Sinan <118401344+sinan0608@users.noreply.github.com>
2023-02-17 00:18:51 +0300

    Recursive Metotlarla Desen

---
+++ RecursiveDesen.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class RecursiveDesen {
+	
+	static void desen(int a ,int b) {
+		if (a>0) {
+			System.out.println(a);
+			desen(a-5, b); //kendisini tekrar çağırdı metotun başına döndü
+		}
+		if (a<=0) {
+			desen2(a, b); // a'nın sıfır veya negatig olan değerini ekrana yazdırıyor.
+		}
+        
+	}
+	static void desen2(int a,int b) { // a değeri b değerine eşit olana metot kadar kendini çağırıyor
+		if (a<=b) {
+			System.out.println(a);
+			desen2(a+5, b);
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Lütfen Bir Sayı Giriniz :");
+		int num =scan.nextInt(); // metot içerisinde ki a değişkenin yerine geçecek
+		int temp = num;          // metot içerisindeki b değişkenin yerine geçecek
+		desen(num,temp);
+		
+	}
+
+}
